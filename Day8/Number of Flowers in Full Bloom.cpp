b84fd2600cd742cbac071d8014194b90ab5202c9
Badal Chaudahry <99376974+badalsingh01@users.noreply.github.com>
2023-10-02 22:08:48 +0530

    Add files via upload

---
+++ Day8/Number of Flowers in Full Bloom.cpp
@@ -0,0 +1,28 @@
+//Number of Flowers in Full Bloom
+//https://leetcode.com/problems/number-of-flowers-in-full-bloom/
+class Solution {
+ public:
+  vector<int> fullBloomFlowers(vector<vector<int>>& flowers,
+                               vector<int>& persons) {
+    vector<int> ans;
+    vector<int> starts;
+    vector<int> ends;
+
+    for (const vector<int>& flower : flowers) {
+      starts.push_back(flower[0]);
+      ends.push_back(flower[1]);
+    }
+
+    sort(starts.begin(), starts.end());
+    sort(ends.begin(), ends.end());
+
+    for (const int p : persons) {
+      const int started =
+          upper_bound(starts.begin(), starts.end(), p) - starts.begin();
+      const int ended = lower_bound(ends.begin(), ends.end(), p) - ends.begin();
+      ans.push_back(started - ended);
+    }
+
+    return ans;
+  }
+};
